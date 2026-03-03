a95a4ef127799880ebd83995f3c13730c7c12902
Your Name <zalatesanalytics@gmail.com>
2026-03-02 22:37:21 -0500

    Initial commit – Zalates media engagement dashboard

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zalates Media Engagement Dashboard
+
+Streamlit dashboard prototype tracking engagement of Ethiopian government ministries on social media.
---
+++ app/streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title='Zalates Media Engagement', layout='wide')
+
+st.title('Zalates Media Engagement Dashboard')
+st.caption('Prototype dashboard for Ethiopian Government social media engagement.')
+
+data = pd.DataFrame({
+    'account': ['MoH', 'MoE', 'MoFA'],
+    'total_engagement': [1200, 950, 700],
+    'avg_engagement_rate': [3.2, 2.7, 1.9],
+})
+
+st.markdown('### Sample Data (placeholder)')
+st.dataframe(data)
+
+st.markdown('### Total Engagement')
+st.bar_chart(data.set_index('account')['total_engagement'])
